7113d13db72daafca726d605276f4c3ba83f9a34
Kendall Koning <kkoning@gmail.com>
2016-09-01 10:52:58 -0400

    Added NullIndividual

---
+++ core/agency/NullIndividual.java
@@ -0,0 +1,13 @@
+package agency;
+
+/**
+ * A NullIndividual has no properties that can evolve. It is useful primarily
+ * when the behavior of an entire species is hard-coded without evolvable
+ * parameters, e.g., an agent that always mimics the behavior of other agents.
+ * 
+ * @author liara
+ *
+ */
+public class NullIndividual extends AbstractIndividual {
+
+}
