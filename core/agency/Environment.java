47deb81e8f56a5b1fe640d25b9719cef10db00b5
Kendall Koning <kkoning@gmail.com>
2016-11-28 14:08:48 -0500

    Revisions to the evolve() loop to execute more operations in parallel

--- core/agency/Environment.java
+++ core/agency/Environment.java
@@ -2,9 +2,15 @@ package agency;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.FutureTask;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import agency.data.*;
+import com.sun.tools.doclint.Env;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -117,8 +123,6 @@ public void writeXMLConfig(Element e) {
     Element element = Config.createUnnamedElement(d, reporter);
     e.appendChild(element);
   }
-
-
 }
 
 @Override
@@ -126,63 +130,120 @@ public void resumeFromCheckpoint() {
 
 }
 
+private static void fitnessAccumulator(
+        Map<Individual, Fitness> collecting,
+        Map<Individual, Fitness> collected) {
+  for (Map.Entry<Individual, Fitness> newEntry : collected.entrySet()) {
+    Fitness existing = collecting.get(newEntry.getKey());
+    if (existing == null) {
+      collecting.put(newEntry.getKey(), newEntry.getValue());
+    } else {
+      existing.combine(newEntry.getValue());
+    }
+  }
+}
+
+private static Map<Individual, Fitness> newFitnessMap() {
+  return new IdentityHashMap<>();
+}
+
 public void evolve() {
 
   Stream<EvaluationGroup> egs = evaluationGroupFactory.createEvaluationGroups(this);
-  Stream<EvaluationGroup> evaluatedGroups = evaluator.evaluate(egs);
-
-  // EvaluationGroups have now been run; do stuff with their data
-  evaluatedGroups.forEach(eg -> {
-
-    // Pull out all the individuals and their fitness samples
-    eg.getResults().forEach(fitMapEnt -> {
-      Individual i = fitMapEnt.getKey();
-      i.addFitnessSample(fitMapEnt.getValue());
-    });
-
-    // Collect data from the agent models
-    // summary data
-    for (ModelSummaryData reporter : modelSummaryDataOutputs) {
-      AgencyData summaryData = eg.getModel().getSummaryData();
-      UUID modelUUID = eg.getId();
-      reporter.writeSummaryData(generation, modelUUID, summaryData);
-    }
-    // per-step data
-    for (ModelPerStepData reporter : modelPerStepDataOutputs) {
-      Map<Integer, AgencyData> perStepData = eg.getPerStepData();
-      UUID modelUUID = eg.getId();
-      reporter.writePerStepData(generation, modelUUID, perStepData);
-    }
-  });
+  List<EvaluationGroup> evaluatedGroups = evaluator.evaluate(egs).collect(Collectors.toList());
 
   // Aggregate fitnesses
-  for (PopulationGroup popGroup : populationGroups) {
-    popGroup.aggregateFitnesses(); // Parallelization inside here
+  Map<Individual, Fitness> aggFitnesses = evaluatedGroups.parallelStream()
+          .map(eg -> eg.getResults()).collect(
+                  Environment::newFitnessMap,
+                  Environment::fitnessAccumulator,
+                  Environment::fitnessAccumulator
+          );
+
+  // Assign fitness
+  aggFitnesses.entrySet().parallelStream().forEach(
+          entry -> entry.getKey().setFitness(entry.getValue()));
+
+  /*
+   * Do data output tasks, in parallel.  There should (ideally) be a 1:1 mapping between
+   * output files and FutureTask objects, so that there are no race conditions in the
+   * data output.  Also, these should all be read-only operations; further processing
+   * that modifies the population state (e.g., reproduction) must take place only after
+   * all of these tasks have finished.
+   */
+  ExecutorService executorService = ForkJoinPool.commonPool();
+  List<FutureTask> outputTasks = new ArrayList<>();
+  for (ModelSummaryData reporter : modelSummaryDataOutputs) {
+    ModelSummaryDataHelper msdh = new ModelSummaryDataHelper(getGeneration(), reporter, evaluatedGroups);
+    FutureTask<Boolean> task = new FutureTask<>(msdh, true);
+    outputTasks.add(task);
+    executorService.execute(task);
   }
-
-  // Do statistics on populations
-  populationGroups.parallelStream().forEach(pg -> {
-    pg.getPopulations().parallelStream().forEach(p -> {
+  for (ModelPerStepData reporter : modelPerStepDataOutputs) {
+    ModelStepDataHelper msdh = new ModelStepDataHelper(generation, reporter, evaluatedGroups);
+    FutureTask<Boolean> task = new FutureTask<>(msdh, true);
+    outputTasks.add(task);
+    executorService.execute(task);
+  }
+  for (PopulationGroup pg : populationGroups) {
+    for (Population p : pg.getPopulations()) {
       for (PopulationData pdo : p.getPopulationDataOutputs()) {
-        pdo.writePopulationData(generation, p);
+        PopulationDataOutputHelper pdoh = new PopulationDataOutputHelper(generation, pdo, p);
+        FutureTask<Boolean> task = new FutureTask<>(pdoh, true);
+        outputTasks.add(task);
+        executorService.execute(task);
       }
-    });
-  });
+    }
+  }
+  for (EnvironmentStatistics es : stats) {
+    EnvStatsOutputHelper esoh = new EnvStatsOutputHelper(es, this);
+    FutureTask<Boolean> task = new FutureTask<>(esoh, true);
+    outputTasks.add(task);
+    executorService.execute(task);
+  }
+  // Wait for the data output tasks to finish.
+  for (FutureTask<Boolean> task : outputTasks) {
+    try {
+      task.get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+  }
+  /*
+   * Data output should now be complete.  It should be safe to modify the population state
+   * for the next generation.
+   */
 
-  // TODO: Balance populations
+
+  // TODO: Balance populations inside of populationGroups.
 
   // Reproduce populations
-  for (PopulationGroup popGroup : populationGroups) {
-    popGroup.reproduce();
-  }
+  populationGroups.parallelStream().forEach(pg -> pg.reproduce());
+  //  for (PopulationGroup popGroup : populationGroups) {
+  //    popGroup.reproduce();
+  //  }
 
-  // Data output
-  for (EnvironmentStatistics es : stats) {
-    es.calculate(this);
-  }
 
   generation++;
 }
+
+private static class EnvStatsOutputHelper implements Runnable {
+  EnvironmentStatistics stats;
+  Environment           env;
+
+  public EnvStatsOutputHelper(EnvironmentStatistics stats, Environment env) {
+    this.stats = stats;
+    this.env = env;
+  }
+
+  @Override
+  public void run() {
+    stats.calculate(env);
+  }
+}
+
 
 public EvaluationGroupFactory getEvaluationGroupFactory() {
   return evaluationGroupFactory;
@@ -218,6 +279,7 @@ public PopulationGroup getPopulationGroup(String id) {
   return null;
 }
 
+
 void addPopulationGroup(PopulationGroup pg) {
   this.populationGroups.add(pg);
 }
@@ -226,53 +288,6 @@ public int getGeneration() {
   return generation;
 }
 
-
-// Stream<Individual> getRandomIndividuals() {
-// PopulationMap indexedPop = new PopulationMap();
-// Collection<Population> pops =
-// getAllPopulations().collect(Collectors.toCollection(ArrayList::new));
-// indexedPop.addPopulations(pops);
-//
-// Random r = ThreadLocalRandom.current();
-//
-// Stream<Individual> toReturn = Stream.generate(() -> (Integer)
-// r.nextInt(indexedPop.totalIndividuals()))
-// .map(i -> indexedPop.getIndividual(i));
-//
-// return toReturn;
-// }
-
-// Stream<Population> getAllPopulations() {
-// Stream<Population> toReturn = Stream.empty();
-// toReturn = Stream.concat(toReturn, populations.stream());
-// if (subEnvironments != null)
-// for (Environment e : subEnvironments)
-// toReturn = Stream.concat(toReturn, e.getAllPopulations());
-// return toReturn;
-// }
-
-/**
- * @return All the individuals present in this environment, regardless of population.
- */
-// Stream<Individual> allIndividuals() {
-// Stream<Individual> toReturn = Stream.empty();
-// for (Population p : populations)
-// toReturn = Stream.concat(toReturn, p.individuals.stream());
-// if (subEnvironments != null)
-// for (Environment e : subEnvironments)
-// toReturn = Stream.concat(toReturn, e.allIndividuals());
-// return toReturn;
-// }
-
-// public Stream<Entry<Individual, Fitness>> allIndividualsWithFitness() {
-// Stream<Entry<Individual, Fitness>> toReturn = Stream.empty();
-// for (Population pop : populations)
-// toReturn = Stream.concat(toReturn, pop.allIndividualsWithFitness());
-// if (subEnvironments.isPresent())
-// for (Environment env : subEnvironments.get())
-// toReturn = Stream.concat(toReturn, env.allIndividualsWithFitness());
-// return toReturn;
-// }
 
 class PopulationMap extends TreeMap<Integer, Population> {
   private static final long serialVersionUID = 4508501876898154219L;
@@ -301,5 +316,65 @@ class PopulationMap extends TreeMap<Integer, Population> {
   }
 
 }
+
+private static class ModelSummaryDataHelper implements Runnable {
+  int                   generation;
+  ModelSummaryData      reporter;
+  List<EvaluationGroup> evaluatedGroups;
+
+  ModelSummaryDataHelper(int generation, ModelSummaryData reporter, List<EvaluationGroup> evaluatedGroups) {
+    this.generation = generation;
+    this.reporter = reporter;
+    this.evaluatedGroups = evaluatedGroups;
+  }
+
+  @Override
+  public void run() {
+    for (EvaluationGroup eg : evaluatedGroups) {
+      AgencyData summaryData = eg.getModel().getSummaryData();
+      UUID modelUUID = eg.getId();
+      reporter.writeSummaryData(generation, modelUUID, summaryData);
+    }
+  }
+}
+
+private static class ModelStepDataHelper implements Runnable {
+  int                   generation;
+  ModelPerStepData      reporter;
+  List<EvaluationGroup> evaluatedGroups;
+
+  ModelStepDataHelper(int generation, ModelPerStepData reporter, List<EvaluationGroup> evaluatedGroups) {
+    this.generation = generation;
+    this.reporter = reporter;
+    this.evaluatedGroups = evaluatedGroups;
+  }
+
+  @Override
+  public void run() {
+    for (EvaluationGroup eg : evaluatedGroups) {
+      Map<Integer, AgencyData> perStepData = eg.getPerStepData();
+      UUID modelUUID = eg.getId();
+      reporter.writePerStepData(generation, modelUUID, perStepData);
+    }
+  }
+}
+
+private static class PopulationDataOutputHelper implements Runnable {
+  int            generation;
+  PopulationData pdo;
+  Population     population;
+
+  public PopulationDataOutputHelper(int generation, PopulationData pdo, Population population) {
+    this.generation = generation;
+    this.pdo = pdo;
+    this.population = population;
+  }
+
+  @Override
+  public void run() {
+    pdo.writePopulationData(generation, population);
+  }
+}
+
 
 }
