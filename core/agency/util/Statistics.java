b2648e97c15ec3feb9799cf1588988d5644218ef
Kendall Koning <kkoning@gmail.com>
2017-01-30 14:15:11 -0500

    Added additional HHI methods for more efficient calculation / more concise usage.

--- core/agency/util/Statistics.java
+++ core/agency/util/Statistics.java
@@ -1,5 +1,6 @@
 package agency.util;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -27,4 +28,62 @@ public static double HHI(Number[] values) {
   }
   return totalSquaredPercent;
 }
+
+public static double HHI(List<? extends Number> values) {
+  double total = 0;
+  double totalSquaredPercent = 0;
+
+  // Start by determining total
+  for (Number value : values) {
+    total += value.doubleValue();
+  }
+
+  // Add squared percentages of each value
+  for (Number value : values) {
+    double percent = (value.doubleValue() / total) * 100;
+    double percentSquared = percent * percent;
+    totalSquaredPercent += percentSquared;
+  }
+  return totalSquaredPercent;
+}
+
+public static double HHI(int[] values) {
+  double total = 0;
+  double totalSquaredPercent = 0;
+
+  // Start by determining total
+  for (int value : values) {
+    total += value;
+  }
+
+  // Add squared percentages of each value
+  for (int value : values) {
+    double doubleValue = (double) value; // cast for floating point division
+    double percent = (doubleValue / total) * 100;
+    double percentSquared = percent * percent;
+    totalSquaredPercent += percentSquared;
+  }
+  return totalSquaredPercent;
+}
+
+public static double HHI(double[] values) {
+  double total = 0;
+  double totalSquaredPercent = 0;
+
+  // Start by determining total
+  for (double value : values) {
+    total += value;
+  }
+
+  // Add squared percentages of each value
+  for (double value : values) {
+    double percent = (value / total) * 100;
+    double percentSquared = percent * percent;
+    totalSquaredPercent += percentSquared;
+  }
+  return totalSquaredPercent;
+}
+
+
+
 }
